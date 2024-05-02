e69894c6bc8df95bc88b3e8d2fa17919659412e0
Basil Goodluck <basilgoodluck22@gmail.com>
2024-05-02 14:25:41 +0100

    header.js updated

--- src/components/header.js
+++ src/components/header.js
@@ -34,32 +34,35 @@ function Header() {
     }, [])
 
     const mobileNav = () => (
-            <div className={` h-screen  p-3 z-40 bg-blue-900 absolute top-0 ${activeNav ? 'w-4/5 left-0' : 'w-0 -left-96'}`} style={{transition: 'ease .5s '}}>
+            <div className={`text-black h-screen pt-4 p-3 z-40 bg-white shadow-2xl absolute top-0 ${activeNav ? 'w-4/5 left-0' : 'w-0 -left-96'}`} style={{transition: 'ease .5s '}}>
                 <div className='w-11/12 m-auto flex justify-between items-center'>
                         <h1 className='font-bold text-2xl' style={{whiteSpace: 'nowrap'}}>Ecommerce App</h1>
                         <div>
                             <p style={{whiteSpace: "nowrap"}}>Back <MdKeyboardArrowRight className='inline'/></p>
                         </div>
                     </div>
-                <nav>
-                    <ul>
-                        <li><a href='./'>Home</a></li>
-                        <li>
+                <nav className='w-11/12 m-auto py-10'>
+                    <ul className='grid gap-4'>
+                        <li className='border-b pt-5 pb-2 text-xl'><a href='./'>Home</a></li>
+                        <li className='border-b pt-5 pb-2 text-xl'>
                             <div>
-                                Categories <RiArrowDropDownLine />
-                                <ul>
-                                    <li><a href='./'>Women's Fashion</a></li>
-                                    <li><a href='./'>Men's Fashion</a></li>
-                                    <li><a href='./'>Electronics</a></li>
-                                    <li><a href='./'>Home & Lifestyle</a></li>
-                                    <li><a href='./'>Medicine</a></li>
-                                    <li><a href='./'>Sports and Outdoor</a></li>
-                                    <li><a href='./'>Baby's and Toys</a></li>
-                                    <li><a href='./'>Groceries and Pets</a></li>
-                                    <li><a href='./'>Health and Beauty</a></li>
+                                Categories <RiArrowDropDownLine className='inline text-2xl'/>
+                                <ul className='h-0 overflow-hidden '>
+                                    <li><a className='text-base p-2' href='./'>Women's Fashion</a></li>
+                                    <li><a className='text-base p-2' href='./'>Men's Fashion</a></li>
+                                    <li><a className='text-base p-2' href='./'>Electronics</a></li>
+                                    <li><a className='text-base p-2' href='./'>Home & Lifestyle</a></li>
+                                    <li><a className='text-base p-2' href='./'>Medicine</a></li>
+                                    <li><a className='text-base p-2' href='./'>Sports and Outdoor</a></li>
+                                    <li><a className='text-base p-2' href='./'>Baby's and Toys</a></li>
+                                    <li><a className='text-base p-2' href='./'>Groceries and Pets</a></li>
+                                    <li><a className='text-base p-2' href='./'>Health and Beauty</a></li>
                                 </ul>
                             </div>
                         </li>
+                        <li className='border-b pt-5 pb-2 text-xl'><a href='./'>Contact</a></li>
+                        <li className='border-b pt-5 pb-2 text-xl'><a href='./'>About</a></li>
+                        <li className='border-b pt-5 pb-2 text-xl'><a href='./'>Sign Up</a></li>
                     </ul>
                 </nav>
                     
