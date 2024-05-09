808581d50430f29b6882e4f4fdf72cd19e24fe1d
Basil Goodluck <basilgoodluck22@gmail.com>
2024-05-10 00:56:54 +0100

    Updated

--- src/components/slider.js
+++ src/components/slider.js
@@ -5,21 +5,26 @@ function Slider() {
   const [currentIndex, setCurrentIndex] = useState(0);
   const sliderBoxRef = useRef(null);
   const [slideWidth, setSlideWidth] = useState(0);
+  const [intervalId, setIntervalId] = useState(null);
 
   const images = [
-    <SlideCard key={0} contentImg={'./assets/teddyBear.webp'} />,
-    <SlideCard key={1} contentImg={'./assets/iphone.png'} />,
-    <SlideCard key={2} contentImg={'./assets/developer.webp'} />,
-    <SlideCard key={3} contentImg={'./assets/teddyBear.webp'} />,
-    <SlideCard key={4} contentImg={'./assets/iphone.png'} />,
+    <SlideCard contentImg={'./assets/iphone.png'} />,
+    <SlideCard contentImg={'./assets/developer.webp'} />,
+    <SlideCard contentImg={'./assets/teddyBear.webp'} />,
   ];
 
+  const clonedFirstSlide = React.cloneElement(images[0])
+  console.log(clonedFirstSlide)
+
   const moveToNextSlide = useCallback(() => {
-    setCurrentIndex(prevIndex => prevIndex === images.length - 1 ? 0 : prevIndex + 1);
-    const targetPosition = -slideWidth * (currentIndex);
-    sliderBoxRef.current.style.transform =  currentIndex === images.length - 1 ? `translateX(0)` : `translateX(${targetPosition}px)`;
-    sliderBoxRef.current.style.transition = currentIndex === images.length - 1 || currentIndex === 0 ? 'none' : 'transform ease .7s';
-  }, [slideWidth, currentIndex, images.length]);
+    setCurrentIndex(prevIndex => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
+    // if(currentIndex ===)
+  }, [images.length]);
+
+  const moveToPrevSlide = useCallback(() => {
+    setCurrentIndex(prevIndex => (prevIndex === 0 ? images.length - 1 : prevIndex - 1));
+  }, [images.length]);
+
 
   useEffect(() => {
     setSlideWidth(sliderBoxRef.current.clientWidth);
@@ -27,25 +32,39 @@ function Slider() {
     const interval = setInterval(() => {
       moveToNextSlide();
     }, 2000);
-    
+
+    setIntervalId(interval);
 
     return () => clearInterval(interval);
-  }, [images.length, slideWidth, moveToNextSlide]);
+  }, [moveToNextSlide]);
+
+  useEffect(() => {
+    if (currentIndex === 0 || currentIndex === images.length - 1) {
+      clearInterval(intervalId);
+    }
+  }, [currentIndex, images.length, intervalId]);
+
+  const handleButtonClick = (direction) => {
+    if (direction === 'prev') {
+      moveToPrevSlide();
+    } else {
+      moveToNextSlide();
+    }
+  };
+
   return (
-    <div className='w-full m-auto overflow-hidden bg-black text-white'>
+    <div className='w-full m-auto overflow-hidden bg-black text-white relative'>
       <div className='bg-black w-full'>
-        <div className='flex w-full justify-between' ref={sliderBoxRef}>
-          {images.map((image) => (
-            image
+        <div className='flex w-full justify-between' ref={sliderBoxRef} style={{ transform: `translateX(${-slideWidth * currentIndex}px)`, transition: `${currentIndex === 0 || currentIndex === images.length - 1 ? 'none' : 'transform ease .7s'}` }}>
+          {[...images, clonedFirstSlide].map((image, index) => (
+            <React.Fragment key={index}>{image}</React.Fragment>
           ))}
         </div>
       </div>
-      <div className='w-full py-1 flex justify-center items-center gap-2'>
-        <button className='w-1 h-1 rounded-full bg-gray-400 p-1'></button>
-        <button className='w-1 h-1 rounded-full bg-gray-400 p-1'></button>
-        <button className='w-1 h-1 rounded-full bg-red-400 p-1'></button>
-        <button className='w-1 h-1 rounded-full bg-gray-400 p-1'></button>
-        <button className='w-1 h-1 rounded-full bg-gray-400 p-1'></button>
+      <div className='w-full flex gap-3 absolute bottom-0 left-3 h-3'>
+        {images.map((_, index) => (
+          <button key={index} className={`slider-button ${currentIndex === index ? 'active' : ''}`} onClick={() => handleButtonClick(index)}></button>
+        ))}
       </div>
     </div>
   );
