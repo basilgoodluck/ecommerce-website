0fb8af05ac07951019017feaf198a6bc5baaeda7
Basil Goodluck <basilgoodluck22@gmail.com>
2024-05-10 16:38:50 +0100

    Updated

--- src/components/slider.js
+++ src/components/slider.js
@@ -1,73 +1,39 @@
-import React, { useEffect, useRef, useState, useCallback } from 'react';
-import SlideCard from './slideCard';
+import React from 'react'
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./slider.css"
 
-function Slider() {
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const sliderBoxRef = useRef(null);
-  const [slideWidth, setSlideWidth] = useState(0);
-  const [intervalId, setIntervalId] = useState(null);
-
-  const images = [
-    <SlideCard contentImg={'./assets/iphone.png'} />,
-    <SlideCard contentImg={'./assets/developer.webp'} />,
-    <SlideCard contentImg={'./assets/teddyBear.webp'} />,
-  ];
-
-  const clonedFirstSlide = React.cloneElement(images[0])
-  console.log(clonedFirstSlide)
-
-  const moveToNextSlide = useCallback(() => {
-    setCurrentIndex(prevIndex => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
-    // if(currentIndex ===)
-  }, [images.length]);
-
-  const moveToPrevSlide = useCallback(() => {
-    setCurrentIndex(prevIndex => (prevIndex === 0 ? images.length - 1 : prevIndex - 1));
-  }, [images.length]);
-
-
-  useEffect(() => {
-    setSlideWidth(sliderBoxRef.current.clientWidth);
-
-    const interval = setInterval(() => {
-      moveToNextSlide();
-    }, 2000);
-
-    setIntervalId(interval);
-
-    return () => clearInterval(interval);
-  }, [moveToNextSlide]);
-
-  useEffect(() => {
-    if (currentIndex === 0 || currentIndex === images.length - 1) {
-      clearInterval(intervalId);
-    }
-  }, [currentIndex, images.length, intervalId]);
-
-  const handleButtonClick = (direction) => {
-    if (direction === 'prev') {
-      moveToPrevSlide();
-    } else {
-      moveToNextSlide();
-    }
+const SliderCase = () => {
+  const settings = {
+    dots: false,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
   };
 
   return (
-    <div className='w-full m-auto overflow-hidden bg-black text-white relative'>
-      <div className='bg-black w-full'>
-        <div className='flex w-full justify-between' ref={sliderBoxRef} style={{ transform: `translateX(${-slideWidth * currentIndex}px)`, transition: `${currentIndex === 0 || currentIndex === images.length - 1 ? 'none' : 'transform ease .7s'}` }}>
-          {[...images, clonedFirstSlide].map((image, index) => (
-            <React.Fragment key={index}>{image}</React.Fragment>
-          ))}
+    <div>
+      <h2>Carousel Component</h2>
+      <Slider {...settings}>
+        <div>
+          <h3>FIRST SLIDE</h3>
         </div>
-      </div>
-      <div className='w-full flex gap-3 absolute bottom-0 left-3 h-3'>
-        {images.map((_, index) => (
-          <button key={index} className={`slider-button ${currentIndex === index ? 'active' : ''}`} onClick={() => handleButtonClick(index)}></button>
-        ))}
-      </div>
+        <div>
+          <h3>SECOND SLIDE</h3>
+        </div>
+        <div>
+          <h3>THIRD SLIDE</h3>
+        </div>
+        <div>
+          <h3>FORTH SLIDE</h3>
+        </div>
+      </Slider>
     </div>
   );
-}
+};
 
-export default Slider;
+export default SliderCase;
--- src/layouts/hero.js
+++ src/layouts/hero.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import Slider from '../components/slider'
+// import Slider from '../components/slider'
 import { MdKeyboardArrowRight } from "react-icons/md";
+import SliderCase from '../components/slider';
 
 function Hero() {
   return (
@@ -8,19 +9,19 @@ function Hero() {
         <div className='w-11/12 md:w-4/5 m-auto flex lg:gap-11 md:gap-3 md:justify-between lg:justify-normal '>
             <div className='hidden md:block border-r border-gray-400 w-1/3 lg:w-2/6 md:w-3/6'>
                 <ul className='h-full flex flex-col justify-between items-start pt-6'>
-                    <li><a className='text-base p-2 flex justify-between' href='./'>Women's Fashion <MdKeyboardArrowRight className='inline text-2xl' /></a></li>
-                    <li><a className='text-base p-2 flex justify-between' href='./'>Men's Fashion <MdKeyboardArrowRight  className='inline text-2xl'/></a></li>
-                    <li><a className='text-base p-2' href='./'>Electronics</a></li>
-                    <li><a className='text-base p-2' href='./'>Home & Lifestyle</a></li>
-                    <li><a className='text-base p-2' href='./'>Medicine</a></li>
-                    <li><a className='text-base p-2' href='./'>Sports and Outdoor</a></li>
-                    <li><a className='text-base p-2' href='./'>Baby's and Toys</a></li>
-                    <li><a className='text-base p-2' href='./'>Groceries and Pets</a></li>
-                    <li><a className='text-base p-2' href='./'>Health and Beauty</a></li>
+                    <li><a className=' text-lg p-2 flex justify-between' href='./'>Women's Fashion <MdKeyboardArrowRight className='inline text-2xl' /></a></li>
+                    <li><a className=' text-lg p-2 flex justify-between' href='./'>Men's Fashion <MdKeyboardArrowRight  className='inline text-2xl'/></a></li>
+                    <li><a className=' text-lg p-2' href='./'>Electronics</a></li>
+                    <li><a className=' text-lg p-2' href='./'>Home & Lifestyle</a></li>
+                    <li><a className=' text-lg p-2' href='./'>Medicine</a></li>
+                    <li><a className=' text-lg p-2' href='./'>Sports and Outdoor</a></li>
+                    <li><a className=' text-lg p-2' href='./'>Baby's and Toys</a></li>
+                    <li><a className=' text-lg p-2' href='./'>Groceries and Pets</a></li>
+                    <li><a className=' text-lg p-2' href='./'>Health and Beauty</a></li>
                 </ul>
             </div>
             <div className='pt-6 m-auto w-full md:w-3/5 '>
-                <Slider />
+                <SliderCase />
             </div>
         </div>
     </div>
